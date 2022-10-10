d2a90aea1ea07df85e5d2463d1e468b1d73ef11f
Fredrik Teschke <f@ftes.de>
2022-10-10 11:16:11 +0200

    ✅ Add seating plan UI tests

--- test/clickr_web/live/seating_plan_live_test.exs
+++ test/clickr_web/live/seating_plan_live_test.exs
@@ -2,7 +2,7 @@ defmodule ClickrWeb.SeatingPlanLiveTest do
   use ClickrWeb.ConnCase
 
   import Phoenix.LiveViewTest
-  import Clickr.{ClassesFixtures, RoomsFixtures}
+  import Clickr.{ClassesFixtures, RoomsFixtures, StudentsFixtures}
 
   @create_attrs %{name: "some name"}
   @update_attrs %{name: "some updated name"}
@@ -94,6 +94,34 @@ defmodule ClickrWeb.SeatingPlanLiveTest do
 
       assert html =~ "Show Seating plan"
       assert html =~ seating_plan.name
+    end
+
+    test "assigns seat to previously unseated student", %{conn: conn, seating_plan: sp} do
+      %{id: sid} = student_fixture(class_id: sp.class_id)
+      {:ok, show_live, _html} = live(conn, ~p"/seating_plans/#{sp}")
+      assert show_live |> has_element?("#unseated-student-#{sid}")
+
+      show_live |> render_hook(:assign_seat, %{x: 1, y: 1, student_id: sid})
+      assert show_live |> has_element?("#seated-student-#{sid}")
+    end
+
+    test "changes student seat", %{conn: conn, seating_plan: sp} do
+      %{id: sid} = student_fixture(class_id: sp.class_id)
+      seating_plan_seat_fixture(seating_plan_id: sp.id, student_id: sid, x: 1, y: 1)
+      {:ok, show_live, _html} = live(conn, ~p"/seating_plans/#{sp}")
+      assert show_live |> has_element?("#seated-student-#{sid}[data-x=1, data-y=1]")
+
+      show_live |> render_hook(:assign_seat, %{x: 2, y: 2, student_id: sid})
+      assert show_live |> has_element?("#seated-student-#{sid}[data-x=2, data-y=2]")
+    end
+
+    test "removes student from seat", %{conn: conn, seating_plan: sp} do
+      %{id: sid} = student_fixture(class_id: sp.class_id)
+      seating_plan_seat_fixture(seating_plan_id: sp.id, student_id: sid, x: 1, y: 1)
+      {:ok, show_live, _html} = live(conn, ~p"/seating_plans/#{sp}")
+
+      show_live |> element("#seated-student-#{sid} button") |> render_click()
+      refute show_live |> has_element?("#seated-student-#{sid}")
     end
 
     test "updates seating_plan within modal", %{conn: conn, seating_plan: seating_plan} do
