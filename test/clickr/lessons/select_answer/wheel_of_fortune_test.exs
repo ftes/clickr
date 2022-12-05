32fd04a8ee7cadc3792c0eded2bf380c91dd5ceb
Fredrik Teschke <f@ftes.de>
2022-12-05 09:21:19 +0100

    ✅ Fix test

--- test/clickr/lessons/select_answer/wheel_of_fortune_test.exs
+++ test/clickr/lessons/select_answer/wheel_of_fortune_test.exs
@@ -9,7 +9,7 @@ defmodule Clickr.Lessons.SelectAnswer.WheelOfFortuneTest do
   end
 
   test "animate_select_answer/1 uses exponentially increasing pauses" do
-    assert [%{pause: 666}, %{pause: 1_000}, %{pause: 1_500}, %{pause: nil}] =
+    assert [%{pause: 444}, %{pause: 666}, %{pause: 1_000}, %{pause: nil}] =
              WheelOfFortune.animate_select_answer(for i <- 1..4, do: %{student_id: i})
   end
 end
